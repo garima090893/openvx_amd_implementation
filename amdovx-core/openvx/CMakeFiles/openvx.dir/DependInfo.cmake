f4e87c68b189f805d931f375a2731802e665fc21
garima090893 <garimakaushik134@gmail.com>
2020-06-18 14:52:26 +0530

    First AMD working implementation

---
+++ amdovx-core/openvx/CMakeFiles/openvx.dir/DependInfo.cmake
@@ -0,0 +1,61 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/ago/ago_drama.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/CMakeFiles/openvx.dir/ago/ago_drama.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/ago/ago_drama_alloc.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/CMakeFiles/openvx.dir/ago/ago_drama_alloc.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/ago/ago_drama_analyze.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/CMakeFiles/openvx.dir/ago/ago_drama_analyze.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/ago/ago_drama_divide.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/CMakeFiles/openvx.dir/ago/ago_drama_divide.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/ago/ago_drama_merge.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/CMakeFiles/openvx.dir/ago/ago_drama_merge.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/ago/ago_drama_remove.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/CMakeFiles/openvx.dir/ago/ago_drama_remove.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/ago/ago_haf_cpu.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/CMakeFiles/openvx.dir/ago/ago_haf_cpu.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/ago/ago_haf_cpu_arithmetic.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/CMakeFiles/openvx.dir/ago/ago_haf_cpu_arithmetic.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/ago/ago_haf_cpu_canny.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/CMakeFiles/openvx.dir/ago/ago_haf_cpu_canny.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/ago/ago_haf_cpu_ch_extract_combine.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/CMakeFiles/openvx.dir/ago/ago_haf_cpu_ch_extract_combine.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/ago/ago_haf_cpu_color_convert.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/CMakeFiles/openvx.dir/ago/ago_haf_cpu_color_convert.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/ago/ago_haf_cpu_fast_corners.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/CMakeFiles/openvx.dir/ago/ago_haf_cpu_fast_corners.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/ago/ago_haf_cpu_filter.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/CMakeFiles/openvx.dir/ago/ago_haf_cpu_filter.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/ago/ago_haf_cpu_geometric.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/CMakeFiles/openvx.dir/ago/ago_haf_cpu_geometric.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/ago/ago_haf_cpu_harris.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/CMakeFiles/openvx.dir/ago/ago_haf_cpu_harris.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/ago/ago_haf_cpu_histogram.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/CMakeFiles/openvx.dir/ago/ago_haf_cpu_histogram.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/ago/ago_haf_cpu_logical.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/CMakeFiles/openvx.dir/ago/ago_haf_cpu_logical.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/ago/ago_haf_cpu_opticalflow.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/CMakeFiles/openvx.dir/ago/ago_haf_cpu_opticalflow.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/ago/ago_haf_cpu_pyramid.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/CMakeFiles/openvx.dir/ago/ago_haf_cpu_pyramid.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/ago/ago_haf_gpu_common.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/CMakeFiles/openvx.dir/ago/ago_haf_gpu_common.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/ago/ago_haf_gpu_conversion.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/CMakeFiles/openvx.dir/ago/ago_haf_gpu_conversion.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/ago/ago_haf_gpu_corners.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/CMakeFiles/openvx.dir/ago/ago_haf_gpu_corners.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/ago/ago_haf_gpu_linear_filter.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/CMakeFiles/openvx.dir/ago/ago_haf_gpu_linear_filter.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/ago/ago_haf_gpu_special_filters.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/CMakeFiles/openvx.dir/ago/ago_haf_gpu_special_filters.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/ago/ago_interface.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/CMakeFiles/openvx.dir/ago/ago_interface.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/ago/ago_kernel_api.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/CMakeFiles/openvx.dir/ago/ago_kernel_api.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/ago/ago_kernel_list.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/CMakeFiles/openvx.dir/ago/ago_kernel_list.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/ago/ago_platform.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/CMakeFiles/openvx.dir/ago/ago_platform.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/ago/ago_util.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/CMakeFiles/openvx.dir/ago/ago_util.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/ago/ago_util_opencl.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/CMakeFiles/openvx.dir/ago/ago_util_opencl.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/api/vx_api.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/CMakeFiles/openvx.dir/api/vx_api.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/api/vx_nodes.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/CMakeFiles/openvx.dir/api/vx_nodes.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/api/vxu.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/CMakeFiles/openvx.dir/api/vxu.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ENABLE_OPENCL=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "openvx/include"
+  "openvx/ago"
+  "openvx/api"
+  "/usr/include/Headers"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ amdovx-core/runvx/CMakeFiles/runvx.dir/DependInfo.cmake
@@ -0,0 +1,97 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/runvx/runvx.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/runvx/CMakeFiles/runvx.dir/runvx.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/runvx/vxArray.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/runvx/CMakeFiles/runvx.dir/vxArray.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/runvx/vxConvolution.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/runvx/CMakeFiles/runvx.dir/vxConvolution.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/runvx/vxDistribution.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/runvx/CMakeFiles/runvx.dir/vxDistribution.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/runvx/vxEngine.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/runvx/CMakeFiles/runvx.dir/vxEngine.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/runvx/vxEngineUtil.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/runvx/CMakeFiles/runvx.dir/vxEngineUtil.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/runvx/vxImage.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/runvx/CMakeFiles/runvx.dir/vxImage.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/runvx/vxLUT.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/runvx/CMakeFiles/runvx.dir/vxLUT.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/runvx/vxMatrix.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/runvx/CMakeFiles/runvx.dir/vxMatrix.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/runvx/vxParamHelper.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/runvx/CMakeFiles/runvx.dir/vxParamHelper.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/runvx/vxParameter.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/runvx/CMakeFiles/runvx.dir/vxParameter.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/runvx/vxPyramid.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/runvx/CMakeFiles/runvx.dir/vxPyramid.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/runvx/vxRemap.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/runvx/CMakeFiles/runvx.dir/vxRemap.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/runvx/vxScalar.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/runvx/CMakeFiles/runvx.dir/vxScalar.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/runvx/vxTensor.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/runvx/CMakeFiles/runvx.dir/vxTensor.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/runvx/vxThreshold.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/runvx/CMakeFiles/runvx.dir/vxThreshold.cpp.o"
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/runvx/vxUtils.cpp" "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/runvx/CMakeFiles/runvx.dir/vxUtils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ENABLE_OPENCL=1"
+  "ENABLE_OPENCV=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "runvx/../openvx/include"
+  "/usr/include/Headers"
+  "/home/venomfate/opencv/build"
+  "/home/venomfate/opencv/include"
+  "/home/venomfate/opencv/include/opencv"
+  "/home/venomfate/opencv/modules/core/include"
+  "/home/venomfate/opencv/modules/flann/include"
+  "/home/venomfate/opencv_contrib/modules/hdf/include"
+  "/home/venomfate/opencv/modules/imgproc/include"
+  "/home/venomfate/opencv/modules/ml/include"
+  "/home/venomfate/opencv_contrib/modules/phase_unwrapping/include"
+  "/home/venomfate/opencv/modules/photo/include"
+  "/home/venomfate/opencv_contrib/modules/plot/include"
+  "/home/venomfate/opencv_contrib/modules/reg/include"
+  "/home/venomfate/opencv_contrib/modules/surface_matching/include"
+  "/home/venomfate/opencv/modules/video/include"
+  "/home/venomfate/opencv_contrib/modules/xphoto/include"
+  "/home/venomfate/opencv/modules/dnn/include"
+  "/home/venomfate/opencv/modules/features2d/include"
+  "/home/venomfate/opencv_contrib/modules/freetype/include"
+  "/home/venomfate/opencv_contrib/modules/fuzzy/include"
+  "/home/venomfate/opencv_contrib/modules/hfs/include"
+  "/home/venomfate/opencv_contrib/modules/img_hash/include"
+  "/home/venomfate/opencv/modules/imgcodecs/include"
+  "/home/venomfate/opencv_contrib/modules/line_descriptor/include"
+  "/home/venomfate/opencv_contrib/modules/saliency/include"
+  "/home/venomfate/opencv/modules/shape/include"
+  "/home/venomfate/opencv_contrib/modules/text/include"
+  "/home/venomfate/opencv/modules/videoio/include"
+  "/home/venomfate/opencv/modules/calib3d/include"
+  "/home/venomfate/opencv_contrib/modules/cvv/include"
+  "/home/venomfate/opencv_contrib/modules/datasets/include"
+  "/home/venomfate/opencv/modules/highgui/include"
+  "/home/venomfate/opencv/modules/objdetect/include"
+  "/home/venomfate/opencv_contrib/modules/rgbd/include"
+  "/home/venomfate/opencv_contrib/modules/stereo/include"
+  "/home/venomfate/opencv_contrib/modules/structured_light/include"
+  "/home/venomfate/opencv/modules/superres/include"
+  "/home/venomfate/opencv_contrib/modules/tracking/include"
+  "/home/venomfate/opencv/modules/ts/include"
+  "/home/venomfate/opencv/modules/videostab/include"
+  "/home/venomfate/opencv_contrib/modules/xfeatures2d/include"
+  "/home/venomfate/opencv_contrib/modules/ximgproc/include"
+  "/home/venomfate/opencv_contrib/modules/xobjdetect/include"
+  "/home/venomfate/opencv_contrib/modules/aruco/include"
+  "/home/venomfate/opencv_contrib/modules/bgsegm/include"
+  "/home/venomfate/opencv_contrib/modules/bioinspired/include"
+  "/home/venomfate/opencv_contrib/modules/ccalib/include"
+  "/home/venomfate/opencv_contrib/modules/dnn_objdetect/include"
+  "/home/venomfate/opencv_contrib/modules/dpm/include"
+  "/home/venomfate/opencv_contrib/modules/face/include"
+  "/home/venomfate/opencv_contrib/modules/optflow/include"
+  "/home/venomfate/opencv_contrib/modules/sfm/include"
+  "/home/venomfate/opencv/modules/stitching/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/venomfate/workspace/office/verisilicon/amd_imp/amdovx-core/openvx/CMakeFiles/openvx.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
